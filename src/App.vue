85f609608ae8c5c274417af1fbd5b23402ceaf05
master <uchixaavi@gmail.com>
2025-06-14 16:18:09 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <SimpleTable :columns="columns" :data="tableData" />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import SimpleTable from './components/SimpleTable.vue'
+
+const tableData = ref([
+  {
+    name: 'momiqvoy',
+    age: 312,
+    address: 'moskva',
+    tags: ['nice', 'programmist']
+  },
+  {
+    name: 'baroqvoy',
+    age: 2312,
+    address: 'doma sidit',
+    tags: ['mushukcha']
+  },
+  {
+    name: 'sherlokvatson',
+    age: 11,
+    address: 'baker strit',
+    tags: ['nice', 'tutor']
+  }
+])
+
+const columns = ref([
+  { title: 'Name', key: 'name' },
+  { title: 'Age', key: 'age' },
+  { title: 'Address', key: 'address' },
+  {
+    title: 'Tags',
+    key: 'tags',
+    customRender: (value) => value.join(', ')
+  }
+])
+</script>
---
+++ src/components/SimpleTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <table border="1" cellspacing="0" cellpadding="5">
+    <thead>
+      <tr>
+        <th v-for="column in columns" :key="column.key">
+          {{ column.title }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in data" :key="index">
+        <td v-for="column in columns" :key="column.key">
+          <span v-if="column.customRender">
+            {{ column.customRender(item[column.key], item) }}
+          </span>
+          <span v-else>
+            {{ item[column.key] }}
+          </span>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+defineProps({
+  data: {
+    type: Array,
+    required: true
+  },
+  columns: {
+    type: Array,
+    required: true
+  }
+})
+</script>
